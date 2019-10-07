c2d172610645ae6c3bc8bc3d67693b8b7165a964
6ry0u <6ry0u@tutanota.com>
2019-10-07 22:59:58 +0200

    Cache vendor assets. Wire html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,28 @@
 const webpack = require('webpack')
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const VENDOR_LIBS = [
+  'faker',
+  'lodash',
+  'react',
+  'react-dom',
+  'react-input-range',
+  'react-redux',
+  'react-router',
+  'redux',
+  'redux-form',
+  'redux-thunk'
+]
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    bundle: './src/index.js',
+    vendor: VENDOR_LIBS
+  },
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: '[name].js'
   },
   module: {
     rules: [
@@ -31,5 +48,15 @@ module.exports = {
         ]
       }
     ]
-  }
+  },
+  /* checks the total sum of our project files
+  between bundle and vendor entry point. If any duplicates, pull these modules/dependencies out and add them only to vendor entry point */
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor'
+    }),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    })
+  ]
 }
